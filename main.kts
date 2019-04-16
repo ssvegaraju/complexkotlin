da85df05b04af9dc4833c1d4b16e5c8ce0e59314
Sukeerth <ssvegaraju@gmail.com>
2019-04-16 00:09:36 -0700

    Finished all parts

--- main.kts
+++ main.kts
@@ -5,8 +5,17 @@ println("UW Complex Kotlin homework")
 // use fold to compress the array of strings down into a single string
 // the final string should look like FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ
 //
-val mapFoldResults = ""
-
+val mapFoldResults = (1..15).map {
+    e ->if (e % 15 == 0) {
+            "FIZZBUZZ"
+        } else if (e % 5 == 0) {
+            "BUZZ"
+        } else if (e % 3 == 0) {
+            "FIZZ"
+        } else {
+            ""
+        }
+}.fold("", {total, next -> total + next})
 
 // This is a utility function for your use as you choose, and as an
 // example of an extension method
@@ -20,16 +29,26 @@ fun Int.times(block: () -> Unit): Unit {
 fun process(message: String, block: (String) -> String): String {
     return ">>> ${message}: {" + block(message) + "}"
 }
-val r1 = "" // call process() with message "FOO" and a block that returns "BAR"
+val r1 = process("FOO", {"BAR"}) // call process() with message "FOO" and a block that returns "BAR"
 
 val r2_message = "wooga"
-val r2 = "" // call process() with message "FOO" and a block that upper-cases 
+val r2 = process("FOO", {r2_message.toUpperCase().repeat(3)}) // call process() with message "FOO" and a block that upper-cases 
             // r2_message, and repeats it three times with no spaces: "WOOGAWOOGAWOOGA"
 
 
 // write an enum-based state machine between talking and thinking
 enum class Philosopher {
-    THINKING
+    THINKING {
+        override fun signal() = TALKING
+        override fun toString() = "Deep thoughts...." // below had four dots so that's what i did despite the spec having 3
+    },
+    TALKING {
+        override fun signal() = THINKING
+        override fun toString() = "Allow me to suggest an idea..."
+    };
+
+    abstract fun signal(): Philosopher
+    override abstract fun toString(): String
  }
 
 // create an class "Command" that can be used as a function (provide an "invoke()" function)
@@ -37,9 +56,10 @@ enum class Philosopher {
 // primary constructor should take a String argument ("prompt")
 // when called, the Command object should return a String containing the prompt and then the message
 class Command(val prompt: String) {
+    operator fun invoke(message: String): String {
+        return prompt + message
+    }
 }
-
-
 
 
 // ================================
